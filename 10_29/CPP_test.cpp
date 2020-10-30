745f97e7d6bc07e3574937f0f75209105c4ab6a9
Shockwave202 <73101679+Shockwave202@users.noreply.github.com>
2020-10-30 08:00:42 +0800

    Update CPP_test.cpp

--- 10_29/CPP_test.cpp
+++ 10_29/CPP_test.cpp
@@ -1,19 +1,20 @@
-#include <Servo.h>
 
-Servo myservo; // create servo object to control a servo
+  Motor motor1,motor2;//Create two motor objects.                
 
-int potpin = 0; // analog pin used to connect the potentiometer
-int val;        // variable to read the value from the analog pin
 
-void setup()
-{
-    myservo.attach(9); // attaches the servo on pin 9 to the servo object
+void setup() {
+  // put your setup code here, to run once:
+motor1.attach(8,5);//Connect the digital pin to the motor driver.
+motor2.attach(7,6);
+
 }
 
-void loop()
-{
-    val = analogRead(potpin);        // reads the value of the potentiometer (value between 0 and 1023)
-    val = map(val, 0, 1023, 0, 180); // scale it to use it with the servo (value between 0 and 180)
-    myservo.write(val);              // sets the servo position according to the scaled value
-    delay(15);                       // waits for the servo to get there
+void loop() {
+  // put your main code here, to run repeatedly:
+
+motor1.rotateClkwise();
+motor2.stop();
+
+
+
 }
